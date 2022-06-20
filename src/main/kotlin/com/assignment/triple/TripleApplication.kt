0eea9fde0dab199ad31944a205143f2983fb871d
funNC_user_Ari <funnc_user@funNC-userui-MacBookPro-2.local>
2022-06-20 16:19:26 +0900

    init commit

---
+++ src/main/kotlin/com/assignment/triple/TripleApplication.kt
@@ -0,0 +1,11 @@
+package com.assignment.triple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripleApplication
+
+fun main(args: Array<String>) {
+	runApplication<TripleApplication>(*args)
+}
